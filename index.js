dda3086ff2af1e35092d1709f91a7b451568ae9d
DocMinike <dominik.majdandzic@gmail.com>
2021-07-07 12:29:42 +0200

    Separated code for scrolling top slide to right into separate function and made it usable for bottom row aswell

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
     $("#nextSlide").on("click", function () {
-        $(".top-row img:last").detach().insertBefore(".top-row img:first");
-        $(".bot-row img:last").detach().insertBefore(".bot-row img:first");
-        let topImgWidth = $(".top-row img:first").width();
-        let columnGap = parseInt($(".slider-row").css("column-gap"));
-        console.log(topImgWidth);
-        $(".top-row img").css({"transform": "translateX(" + (topImgWidth + columnGap) + "px)", "right": 0});
-
-        $(".top-row img").animate({ right: (topImgWidth + columnGap) + "px" }, 100);
-        
+        scrollSlide(".top-row img");
+        scrollSlide(".bot-row img");
     });
 
     $("#prevSlide").on("click", function () {
         $(".top-row img:first").detach().insertAfter(".top-row img:last");
         $(".bot-row img:first").detach().insertAfter(".bot-row img:last");
     });
+
+    function scrollSlide(slideImages) {
+        $(slideImages + ":last").detach().insertBefore(slideImages + ":first");
+        let imgWidth = $(slideImages + ":first").width();
+        let columnGap = parseInt($(".slider-row").css("column-gap"));
+        console.log(imgWidth);
+        $(slideImages).css({ "transform": "translateX(" + (imgWidth + columnGap) + "px)", "right": 0 });
+        $(slideImages).animate({ right: (imgWidth + columnGap) + "px" }, 100);
+    }
 });
