798cded3cffbfaf6809b2b6e53b95a4a4430e27f
DocMinike <dominik.majdandzic@gmail.com>
2021-07-07 11:18:04 +0200

    Added functionality for scrolling slide to the left (without animations)

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
         $(".top-row img:last").detach().insertBefore(".top-row img:first");
         $(".bot-row img:last").detach().insertBefore(".bot-row img:first");
     });
+
+    $("#prevSlide").on("click", function () {
+        $(".top-row img:first").detach().insertAfter(".top-row img:last");
+        $(".bot-row img:first").detach().insertAfter(".bot-row img:last");
+    });
 });
