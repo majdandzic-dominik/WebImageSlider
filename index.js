8f2debac42fc76cdd532ee7e2fa83a045960b270
DocMinike <dominik.majdandzic@gmail.com>
2021-07-07 12:23:48 +0200

    Added animation for scrolling top slide to the right

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ $(document).ready(function () {
     $("#nextSlide").on("click", function () {
         $(".top-row img:last").detach().insertBefore(".top-row img:first");
         $(".bot-row img:last").detach().insertBefore(".bot-row img:first");
+        let topImgWidth = $(".top-row img:first").width();
+        let columnGap = parseInt($(".slider-row").css("column-gap"));
+        console.log(topImgWidth);
+        $(".top-row img").css({"transform": "translateX(" + (topImgWidth + columnGap) + "px)", "right": 0});
+
+        $(".top-row img").animate({ right: (topImgWidth + columnGap) + "px" }, 100);
+        
     });
 
     $("#prevSlide").on("click", function () {
