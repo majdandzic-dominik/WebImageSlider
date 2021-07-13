0edc66cbaa8b489dc1c251223de7fe72b206626b
DocMinike <dominik.majdandzic@gmail.com>
2021-07-13 14:36:32 +0200

    Buttons are now disabled during slider animation so that the animation doesn't glitch if buttons are pressed too fast

--- index.js
+++ index.js
@@ -1,34 +1,51 @@
 $(document).ready(function () {
-    $("#nextSlide").on("click", function () {
+
+    let animationDuration = 200;
+
+    $("#btnScrollRight").on("click", function () {
         scrollSlideToRight(".top-row img");
         scrollSlideToRight(".bot-row img");
     });
 
-    $("#prevSlide").on("click", function () {
+    $("#btnScrollLeft").on("click", function () {
         scrollSlideToLeft(".top-row img");
         scrollSlideToLeft(".bot-row img");
     });
 
-    function scrollSlideToLeft(slideImages) {
+    function setButtonDisabled(isDisabled) {
+        $("#btnScrollRight").prop("disabled", isDisabled);
+        $("#btnScrollLeft").prop('disabled', isDisabled);
+    }
+
+    function scrollSlideToRight(slideImages) {
+        setButtonDisabled(true);
+
+
         $(slideImages + ":last").detach().insertBefore(slideImages + ":first");
         let imgWidth = $(slideImages + ":first").width();
         let columnGap = parseInt($(".slider-row").css("column-gap"));
 
         $(slideImages).css({ "transform": "translateX(" + (imgWidth + columnGap) + "px)", "right": 0, "left": "" });
-        $(slideImages).animate({ right: (imgWidth + columnGap) + "px" }, 200);
+        $(slideImages).animate({ right: (imgWidth + columnGap) + "px" }, animationDuration, function () { setButtonDisabled(false); });
+
+
     }
 
-    function scrollSlideToRight(slideImages) {
+    function scrollSlideToLeft(slideImages) {
+        setButtonDisabled(true);
+
         let imgWidth = $(slideImages + ":first").width();
         let columnGap = parseInt($(".slider-row").css("column-gap"));
 
         $(slideImages).css({ "right": "", "transform": "" });
 
-        $(slideImages).animate({ left: (imgWidth + columnGap) + "px" }, 200);
+        $(slideImages).animate({ left: (imgWidth + columnGap) + "px" }, animationDuration, function () { setButtonDisabled(false); });
         $(slideImages).promise().done(function () {
             $(slideImages + ":first").detach().insertAfter(slideImages + ":last");
             $(slideImages).css({ "left": 0 });
         });
+
+
 
     }
 });
