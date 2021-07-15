4ea94f7c3b83bc865aa9ae1a3704d8b41ce54da5
DocMinike <dominik.majdandzic@gmail.com>
2021-07-15 11:13:58 +0200

    In index.js changed function syntax to arrow function syntax.

--- index.js
+++ index.js
@@ -12,33 +12,33 @@ $(document).ready(function () {
         scrollSlideToLeft(".bot-row img");
     });
 
-    function setButtonDisabled(isDisabled) {
+    const setButtonDisabled = (isDisabled) => {
         $("#btnScrollRight").prop("disabled", isDisabled);
         $("#btnScrollLeft").prop('disabled', isDisabled);
     }
 
-    function scrollSlideToRight(slideImages) {
+    const scrollSlideToRight = (slideImages) => {
         setButtonDisabled(true);
-    
+
         let imgWidth = $(slideImages + ":first").width();
         let columnGap = parseInt($(".slider-row").css("column-gap"));
-    
+
         $(slideImages).css({ "right": "", "transform": "" });
-    
+
         $(slideImages).animate({ left: (imgWidth + columnGap) + "px" }, animationDuration, function () { setButtonDisabled(false); });
         $(slideImages).promise().done(function () {
             $(slideImages + ":first").detach().insertAfter(slideImages + ":last");
             $(slideImages).css({ "left": 0 });
-        }); 
+        });
     }
-    
-    function scrollSlideToLeft(slideImages) {
+
+    const scrollSlideToLeft = (slideImages) => {
         setButtonDisabled(true);
-    
+
         $(slideImages + ":last").detach().insertBefore(slideImages + ":first");
         let imgWidth = $(slideImages + ":first").width();
         let columnGap = parseInt($(".slider-row").css("column-gap"));
-    
+
         $(slideImages).css({ "transform": "translateX(" + (imgWidth + columnGap) + "px)", "right": 0, "left": "" });
         $(slideImages).animate({ right: (imgWidth + columnGap) + "px" }, animationDuration, function () { setButtonDisabled(false); });
     }
