097c08720328ed50ba320a7b49bb8e9048bc4693
DocMinike <dominik.majdandzic@gmail.com>
2021-07-26 12:30:30 +0200

    Removed console loggers used for debugging

--- index.js
+++ index.js
@@ -23,27 +23,25 @@ const setButtonDisabled = (isDisabled) => {
 
 const scrollSlideToLeft = (slideImages) => {
     setButtonDisabled(true);
-    console.log(1);
+
     let imgList = slideImages.children;
     let numOfImgs = imgList.length;
     let firstImg = imgList[0];
     let lastImg = imgList[numOfImgs - 1];
     let imgWidth = lastImg.clientWidth;
-    console.log(2);
+
     //put last image to the begining and shift images to the right instantly without animation
     slideImages.classList.add("no-transitions");
     slideImages.style.transform = "translateX(" + (imgWidth + sliderColumnGap) + "px)";
     slideImages.insertBefore(lastImg, firstImg);
-    console.log(3);
+
     //slide images to the left with animation
     setTimeout(() => {
         slideImages.classList.remove("no-transitions")
         slideImages.style.transform = "translateX(" + 0 + "px)";
-        console.log(4);
     }, 1);
 
     slideImages.addEventListener("transitionend", function handler(e) {
-        console.log(5);
         setButtonDisabled(false);
         e.currentTarget.removeEventListener(e.type, handler);
     });
