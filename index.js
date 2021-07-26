3c14a0ff2e7d8ec2f3ed7a25ca5ba31e292c80d2
DocMinike <dominik.majdandzic@gmail.com>
2021-07-26 12:23:02 +0200

    Changed jqeury syntax to ES6. One issue that needs to be fixed after updating css is that the slider images overlap with the text during animation.

--- index.js
+++ index.js
@@ -1,45 +1,73 @@
-$(document).ready(function () {
+const sliderColumnGap = 9;
 
-    let animationDuration = 200;
+const btnLeft = document.querySelector("#btnScrollLeft");
+const btnRight = document.querySelector("#btnScrollRight");
 
-    $("#btnScrollRight").on("click", function () {
-        scrollSlideToRight(".top-row img");
-        scrollSlideToRight(".bot-row img");
-    });
+const topRowImages = document.querySelector(".top-row");
+const botRowImages = document.querySelector(".bot-row");
 
-    $("#btnScrollLeft").on("click", function () {
-        scrollSlideToLeft(".top-row img");
-        scrollSlideToLeft(".bot-row img");
-    });
-
-    const setButtonDisabled = (isDisabled) => {
-        $("#btnScrollRight").prop("disabled", isDisabled);
-        $("#btnScrollLeft").prop('disabled', isDisabled);
-    }
-
-    const scrollSlideToRight = (slideImages) => {
-        setButtonDisabled(true);
-
-        let imgWidth = $(slideImages + ":first").width();
-        let columnGap = parseInt($(".slider-row").css("column-gap"));
-
-        $(slideImages).css({ "right": "", "transform": "" });
-
-        $(slideImages).animate({ left: (imgWidth + columnGap) + "px" }, animationDuration, function () { setButtonDisabled(false); });
-        $(slideImages).promise().done(function () {
-            $(slideImages + ":first").detach().insertAfter(slideImages + ":last");
-            $(slideImages).css({ "left": 0 });
-        });
-    }
-
-    const scrollSlideToLeft = (slideImages) => {
-        setButtonDisabled(true);
-
-        $(slideImages + ":last").detach().insertBefore(slideImages + ":first");
-        let imgWidth = $(slideImages + ":first").width();
-        let columnGap = parseInt($(".slider-row").css("column-gap"));
-
-        $(slideImages).css({ "transform": "translateX(" + (imgWidth + columnGap) + "px)", "right": 0, "left": "" });
-        $(slideImages).animate({ right: (imgWidth + columnGap) + "px" }, animationDuration, function () { setButtonDisabled(false); });
-    }
+btnLeft.addEventListener("click", () => {
+    scrollSlideToLeft(topRowImages);
+    scrollSlideToLeft(botRowImages);
 });
+
+btnRight.addEventListener("click", () => {
+    scrollSlideToRight(topRowImages);
+    scrollSlideToRight(botRowImages);
+});
+
+const setButtonDisabled = (isDisabled) => {
+    btnLeft.disabled = isDisabled;
+    btnRight.disabled = isDisabled;
+}
+
+const scrollSlideToLeft = (slideImages) => {
+    setButtonDisabled(true);
+    console.log(1);
+    let imgList = slideImages.children;
+    let numOfImgs = imgList.length;
+    let firstImg = imgList[0];
+    let lastImg = imgList[numOfImgs - 1];
+    let imgWidth = lastImg.clientWidth;
+    console.log(2);
+    //put last image to the begining and shift images to the right instantly without animation
+    slideImages.classList.add("no-transitions");
+    slideImages.style.transform = "translateX(" + (imgWidth + sliderColumnGap) + "px)";
+    slideImages.insertBefore(lastImg, firstImg);
+    console.log(3);
+    //slide images to the left with animation
+    setTimeout(() => {
+        slideImages.classList.remove("no-transitions")
+        slideImages.style.transform = "translateX(" + 0 + "px)";
+        console.log(4);
+    }, 1);
+
+    slideImages.addEventListener("transitionend", function handler(e) {
+        console.log(5);
+        setButtonDisabled(false);
+        e.currentTarget.removeEventListener(e.type, handler);
+    });
+}
+
+const scrollSlideToRight = (slideImages) => {
+    setButtonDisabled(true);
+
+    let imgList = slideImages.children;
+    let numOfImgs = imgList.length;
+    let firstImg = imgList[0];
+    let lastImg = imgList[numOfImgs - 1];
+    let imgWidth = firstImg.clientWidth;
+
+    //slide images to the right with animation
+    slideImages.style.transform = "translateX(" + (imgWidth + sliderColumnGap) + "px)";
+
+    //move first image to the end and shift images back to initial state without animation
+    slideImages.addEventListener("transitionend", function handler(e) {
+        slideImages.classList.add("no-transitions");
+        slideImages.insertBefore(firstImg, lastImg.nextSibling);
+        slideImages.style.transform = "translateX(" + 0 + "px)";
+        setTimeout(() => { slideImages.classList.remove("no-transitions") }, 1);
+        setButtonDisabled(false);
+        e.currentTarget.removeEventListener(e.type, handler);
+    });
+}
